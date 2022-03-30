d8d36c33cd9af758a5f96a12648043134d5b7038
liuyang <yang.liu2@northeastern.edu>
2022-03-29 21:49:17 -0400

    update main activity

---
+++ app/src/main/java/edu/neu/madcourse/cs5520_explorer_final_Datinder/CreateAccountActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.cs5520_explorer_final_Datinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/cs5520_explorer_final_Datinder/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/cs5520_explorer_final_Datinder/MainActivity.java
@@ -2,13 +2,39 @@ package edu.neu.madcourse.cs5520_explorer_final_Datinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button button;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.create_account);
+        button.setOnClickListener(this);
+        button = findViewById(R.id.sign_in);
+        button.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.create_account:
+                intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.sign_in:
+                intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
