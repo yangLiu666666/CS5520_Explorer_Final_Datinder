d1f01513dc6cd9cb3df2892100b466f5e4464050
liuyang <yang.liu2@northeastern.edu>
2022-04-04 18:10:36 -0400

    add sign in

---
+++ app/src/main/java/edu/neu/madcourse/cs5520_explorer_final_Datinder/MatchScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.cs5520_explorer_final_Datinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_screen);
+    }
+}
