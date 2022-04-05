954f5f8c0757e5b96ffa9e14f9bd225527d7d344
Nicole Ni <shallwaytalk@gmail.com>
2022-04-04 17:57:19 -0700

    Init Profile activity

---
+++ app/src/main/java/edu/neu/madcourse/cs5520_explorer_final_Datinder/Profile/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.cs5520_explorer_final_Datinder.Profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.neu.madcourse.cs5520_explorer_final_Datinder.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
